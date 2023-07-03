d8e703909263a6cc2a33e32851662cf4e13a3e97
leloufadel <leloufadel@gmail.com>
2023-07-03 16:25:22 +0300

    display movie names from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+//API_KEY = '40xQQTkeHEs9D9wNQTQX0YGfyh2aoT-w';
+const fetchAPI = async () => {
+  const baseURL = 'https://api.tvmaze.com/shows';
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  const shows = data.slice(0, 15); // Limit to shows
+
+  // Display the show list in the HTML
+  const showListContainer = document.getElementById('show-list');
+  shows.forEach(show => {
+    const showTitle = show.name;
+    const showURL = show.url;
+    const showImage = show.image.medium; 
+
+    const listItem = document.createElement('li');
+    listItem.classList.add('shows-movie');
+    listItem.innerHTML = `
+      <h2>${showTitle}</h2>
+      <img src="${showImage}" alt="${showTitle}">
+     
+      <a href="${showURL}" target="_blank">${showURL}</a>
+      <hr>
+    `;
+
+    showListContainer.appendChild(listItem);
+  });
+};
+
+fetchAPI().catch(error => console.log(error));
